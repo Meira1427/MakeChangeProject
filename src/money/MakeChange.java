7ea9ed130342fc446d80fb87ae49fb659ee99be3
Meira Pentermann <meira@meirapentermann.com>
2017-08-06 07:49:25 -0600

    change to int array

--- src/money/MakeChange.java
+++ src/money/MakeChange.java
@@ -1,12 +1,9 @@
-/*In the cash register we will calculate the amount of 
-change returned to a customer based on the purchase price 
-and the amount tendered. We will also notify the attendant 
-how many of each piece of currency 
-($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed 
-to make the change for the customer. Change should be 
-provided using the largest bill and coin denominations 
-as possible. Denominations that are not used should not 
-be displayed.*/
+/*In the cash register we will calculate the amount of change returned to a customer 
+ * based on the purchase price and the amount tendered. We will also notify the 
+ * attendant how many of each piece of currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) 
+ * is needed to make the change for the customer. Change should be provided using the largest 
+ * bill and coin denominations as possible. Denominations that are not used should not 
+ * be displayed.*/
 
 package money;
 
@@ -17,8 +14,13 @@ public class MakeChange {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		double cost, amountTendered;
-		int numTwenty, numTen, numFive, numOne;
-		int numQuarter, numDime, numNickel, numPenny;
+		/* 
+		 * Change this to int array
+		 * numTwenty, numTen, numFive, numOne;
+		 * numQuarter, numDime, numNickel, numPenny;
+		 */
+		int [] changeBreakdown = new int[8];
+		
 		cost = getCleanDouble(keyboard, "Enter the cost of your item: ");
 		cost = roundDouble(cost);
 		amountTendered = getCleanDouble(keyboard, "Enter amount tendered: ");
